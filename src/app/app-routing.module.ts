c68688e82d8870c2e556b81994c67e9080d2f3a2
jeremie <alcaraz.jeremie@hotmail.fr>
2019-08-09 13:01:11 +0200

    add 3 path :   {path: 'books/new', component: BookFormComponent},                {path: 'books/:id', component: SingleBookComponent},                {path: 'books/user/:borrowerId', component: BookReserveComponent},

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import {LoginComponent} from './login/login.component';
 import {AppComponent} from './app.component';
 import {BooksComponent} from './books/books.component';
 import {AdminBooksComponent} from './admin-books/admin-books.component';
+import {BookFormComponent} from './book-list/book-form/book-form.component';
+import {SingleBookComponent} from './book-list/single-book/single-book.component';
+import {BookReserveComponent} from './book-list/book-reserve/book-reserve.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'books', component: BooksComponent},
-
+  {path: 'books/new', component: BookFormComponent},
+  {path: 'books/:id', component: SingleBookComponent},
+  {path: 'books/user/:borrowerId', component: BookReserveComponent},
 ];
 
 @NgModule({
